5d55df1cc0c9dde5951c6d75385b8659a3b90225
Cesare Rossi <cesare.rossi@terradue.co>
2018-08-16 15:28:06 +0200

    Added send_issue_create method

--- app/controllers/elasticsearch_notifier_controller.rb
+++ app/controllers/elasticsearch_notifier_controller.rb
@@ -11,26 +11,51 @@ class ElasticsearchNotifierController < ApplicationController
 
     context[:journal].details.each do |j|
       changes.push({
-        "property" => j.prop_key,
-        "old_value" => j.old_value,
-        "value" => j.value
+        "property"  => j.prop_key,
+        "value"     => j.value,
+        "old_value" => j.old_value
       })
     end
 
     post_to_server({
-        "type"    => "issue",
-        "action"  => "update",
-        "user"    => u.to_json,
-        "id"      => context[:issue].id,
-        "notes"   => context[:journal].notes,
-        "changes" => changes.to_json,
+        "type"       => "issue",
+        "action"     => "update",
+        "user"       => u.to_json,
+        "issue_id"   => context[:issue].id,
+        "updated_on" => context[:issue].updated_on,
+        "notes"      => context[:journal].notes,
+        "changes"    => changes.to_json
     })
 
+  end
+
+  def self.send_issue_create(user, context)
+
+    u = {
+      "email"     => user.mail,
+      "firstname" => user.firstname,
+      "lastname"  => user.lastname
+    }
+
+    info = {
+        "type"   => "issue",
+        "action" => "create",
+        "user"   => u.to_json
+    }
+
+    data = JSON.parse(info.to_json).merge(JSON.parse(context[:issue].to_json))
+
+    # renaming id to avoid confusion with Elasticsearch's _id field
+    # https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-id-field.html
+    data[:issue_id] = data.delete("id")
+
+    post_to_server(JSON.dump(data))
   end
 private
   def self.elasticsearch_rest_endpoint()
     return Setting.plugin_elasticsearch_notifier['elasticsearch_rest_endpoint']
   end
+
   def self.post_to_server(data)
     Rails.logger.info("ELASTICSEARCH_NOTIFIER: Posting entry to " + self.elasticsearch_rest_endpoint + ": " + data.to_json)
   end
--- lib/elasticsearch_notifier_issue_listener.rb
+++ lib/elasticsearch_notifier_issue_listener.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 class ElasticsearchNotifierIssueListener < Redmine::Hook::Listener
   def controller_issues_new_after_save(context={})
     if context[:issue]
-      ElasticsearchNotifierController.send_issue_update(User.current, context)
+      ElasticsearchNotifierController.send_issue_create(User.current, context)
     end
   end
   def controller_issues_edit_after_save(context={})
