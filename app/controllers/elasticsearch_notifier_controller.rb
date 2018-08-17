dd6347dadd7013cd4a82ba66e6f672dff2f4c08a
Cesare Rossi <cesare.rossi@terradue.co>
2018-08-17 08:39:13 +0200

    Adding exception handling

--- app/controllers/elasticsearch_notifier_controller.rb
+++ app/controllers/elasticsearch_notifier_controller.rb
@@ -6,56 +6,65 @@ require 'json'
 class ElasticsearchNotifierController < ApplicationController
 
   def self.send_issue_create(user, context)
+    begin
+      u = {
+        "email"     => user.mail,
+        "firstname" => user.firstname,
+        "lastname"  => user.lastname
+      }
 
-    u = {
-      "email"     => user.mail,
-      "firstname" => user.firstname,
-      "lastname"  => user.lastname
-    }
-
-    info = {
+      info = {
         "resource" => "issue",
         "action"   => "create",
         "user"     => u.to_json
-    }
+      }
 
-    data = JSON.parse(info.to_json).merge(JSON.parse(context[:issue].to_json))
+      data = JSON.parse(info.to_json).merge(JSON.parse(context[:issue].to_json))
 
-    # renaming 'id' to 'issue_id' in order to avoid confusion with Elasticsearch's _id field
-    # https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-id-field.html
-    data[:issue_id] = data.delete("id")
+      # renaming 'id' to 'issue_id' in order to avoid confusion with Elasticsearch's _id field
+      # https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-id-field.html
+      data[:issue_id] = data.delete("id")
 
-    post_to_server(JSON.dump(data))
+      post_to_server(JSON.dump(data))
+    rescue StandardError => e
+      Rails.logger.info e.class.to_s
+      Rails.logger.info e.to_s
+      Rails.logger.info e.backtrace.join("\n")
+    end
   end
   
   def self.send_issue_update(user, context)
-
-    u = {
-      "email"     => user.mail,
-      "firstname" => user.firstname,
-      "lastname"  => user.lastname
-    }
+    begin
+      u = {
+        "email"     => user.mail,
+        "firstname" => user.firstname,
+        "lastname"  => user.lastname
+      }
     
-    changes = []
+      changes = []
 
-    context[:journal].details.each do |j|
-      changes.push({
-        "property"  => j.prop_key,
-        "value"     => j.value,
-        "old_value" => j.old_value
+      context[:journal].details.each do |j|
+        changes.push({
+          "property"  => j.prop_key,
+          "value"     => j.value,
+          "old_value" => j.old_value
+        })
+      end
+
+      post_to_server({
+          "resource"   => "issue",
+          "action"     => "update",
+          "user"       => u.to_json,
+          "issue_id"   => context[:issue].id,
+          "updated_on" => context[:issue].updated_on,
+          "notes"      => context[:journal].notes,
+          "changes"    => changes.to_json
       })
+    rescue StandardError => e
+      Rails.logger.info e.class.to_s
+      Rails.logger.info e.to_s
+      Rails.logger.info e.backtrace.join("\n")
     end
-
-    post_to_server({
-        "resource"   => "issue",
-        "action"     => "update",
-        "user"       => u.to_json,
-        "issue_id"   => context[:issue].id,
-        "updated_on" => context[:issue].updated_on,
-        "notes"      => context[:journal].notes,
-        "changes"    => changes.to_json
-    })
-
   end
 private
   def self.elasticsearch_rest_endpoint()
@@ -63,17 +72,27 @@ private
   end
 
   def self.post_to_server(data)
-    uri = URI(self.elasticsearch_rest_endpoint)
+    begin
+      uri = URI(self.elasticsearch_rest_endpoint)
 
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request["Content-Type"] = "application/json"
-    request.body = data.to_json
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request["Content-Type"] = "application/json"
+      request["Content-Encoding"] = "text"
+      request.body = data.to_json
 
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = (uri.scheme == "https")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = (uri.scheme == "https")
 
-    response = http.request(request)
+      response = http.request(request)
 
-    Rails.logger.info("ELASTICSEARCH_NOTIFIER: Elasticsearch's server response " + response.body)
+      Rails.logger.info("ELASTICSEARCH_NOTIFIER: Payload " + data.to_json)
+      Rails.logger.info("ELASTICSEARCH_NOTIFIER: Elasticsearch's server response " + response.body)
+      
+      raise Net::HTTPBadResponse.new(response.body) if response.code != 200
+    rescue StandardError => e
+      Rails.logger.info e.class.to_s
+      Rails.logger.info e.to_s
+      Rails.logger.info e.backtrace.join("\n")
+    end
   end
 end
